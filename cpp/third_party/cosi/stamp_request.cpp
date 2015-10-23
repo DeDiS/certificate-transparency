bf57deeb99e3c4dc69cb840c10279dec393f9776
Linus Gasser <linus.gasser@epfl.ch>
2015-10-23 13:43:26 +0200

    removed couts

--- cpp/third_party/cosi/stamp_request.cpp
+++ cpp/third_party/cosi/stamp_request.cpp
@@ -38,23 +38,17 @@ namespace Cosi{
   string requestSignature(char *host, int port, string msg ){
     int m_sock = connectTo(host, port);
     if ( m_sock < 0 ){
-      cout << "Connection error\n";
       return NULL;
     }
 
     string request = json_request + msg + json_request_end;
-    cout << "Sending message: " + request + "\n";
     if ( writeString(m_sock, request) < 0 ){
-      cout << "Error while writing signing request\n";
       return NULL;
     }
 
     string signature = readString(m_sock);;
-    cout << "Got string: " << signature;
 
-    cout << "Sending message: " + json_close;
     if ( writeString(m_sock, json_close) < 0 ){
-      cout << "Error while asking to close\n";
       return NULL;
     }
     return signature;
@@ -65,14 +59,12 @@ namespace Cosi{
     int m_sock = socket ( AF_INET, SOCK_STREAM, 0 );
 
     if ( m_sock == -1 ){
-      cout << "could't create socket\n";
       return -1;
     }
 
     // TIME_WAIT - argh
     int on = 1;
     if ( setsockopt ( m_sock, SOL_SOCKET, SO_REUSEADDR, ( const char* ) &on, sizeof ( on ) ) == -1 ){
-      cout << "could't connect\n";
       return -1;
     }
 
@@ -86,7 +78,6 @@ namespace Cosi{
     status = ::connect ( m_sock, ( sockaddr * ) &m_addr, sizeof ( m_addr ) );
 
     if ( status < 0 ){
-      cout << "could't connect\n";
       return -1;
     }
     return m_sock;
@@ -108,7 +99,6 @@ namespace Cosi{
     memset ( buf, 0, MAXRECV + 1 );
     int status = ::recv ( m_sock, buf, MAXRECV, 0 );
     if ( status == -1 ){
-      cout << "status == -1   errno == " << errno << "  in Socket::recv\n";
       free(buf);
       return NULL;
     }
@@ -123,9 +113,6 @@ namespace Cosi{
       char byte = (char) strtol(byteString.c_str(), NULL, 16);
       bytes[i / 2] = byte;
     }
-
-    cout << hex.length() / 2;
-    cout << " bytes converted\n";
 
     return bytes;
   }
