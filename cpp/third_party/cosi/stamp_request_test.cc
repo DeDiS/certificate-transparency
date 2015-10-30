3df3b0472ffd0ff0e9dee605c0d9a3d6a35dca67
Linus Gasser <linus.gasser@epfl.ch>
2015-10-30 14:07:53 +0100

    normalized test

--- cpp/third_party/cosi/stamp_request_test.cc
+++ cpp/third_party/cosi/stamp_request_test.cc
@@ -9,21 +9,35 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include "stamp_request.h"
+#include "util/testing.h"
+#include "util/status_test_util.h"
+
+class StampRequestTest : public ::testing::Test {
+protected:
+  StampRequestTest(){
+  }
+
+  void SetUp() {
+  }
+};
 
 using namespace std;
 
-int main(){
-  const unsigned char bytes[5]={0x00, 0x40, 0x80, 0xc0, 0xff};
-  cout << "Conversion: " << Cosi::BytesToHex(bytes, 5) << "\n";
-
+TEST_F(StampRequestTest, SignSTH){
   string test;
-  cout << "Requesting signature\n";
-  cout << Cosi::requestSignature("localhost", 2011, " 0 1 0 0 150FFFFFF56 0 0 0 0 0 0 0 0FFFFFF42FFFF1C14FFFFFFFFFF6FFF2427FF41FF64FFFF4CFFFFFF1B7852FF55" );
+  VLOG(2) << "Requesting signature\n";
+  VLOG(2) << Cosi::requestSignature("localhost", 2011, " 0 1 0 0 150FFFFFF56 0 0 0 0 0 0 0 0FFFFFF42FFFF1C14FFFFFFFFFF6FFF2427FF41FF64FFFF4CFFFFFF1B7852FF55" );
 
   ct::SignedTreeHead sth;
   sth.set_timestamp(1000);
-  cout << "Asking STH to be signed\n";
-  cout << Cosi::SignTreeHead( &sth );
-  cout << "Signature received\n";
-  return 0;
+  VLOG(2) << "Asking STH to be signed\n";
+  string sth_signed = Cosi::SignTreeHead( &sth );
+  VLOG(2) << sth_signed;
+  ASSERT_NE(sth_signed, "");
+  VLOG(2) << "Signature received\n";
+}
+
+int main(int argc, char** argv) {
+  cert_trans::test::InitTesting(argv[0], &argc, &argv, true);
+  return RUN_ALL_TESTS();
 }
